4ee7e448b3a6de153f533895f131fc3fb9806184
Narongsuk Sihawong <toom_phy@hotmail.com>
2018-03-19 15:45:31 +0700

    Update tests.ts

--- tests.ts
+++ tests.ts
@@ -1,43 +1,43 @@
-input.onButtonPressed(Button.A, () => {
-    IBIT.Turn(turn.Left, 50)
+https://github.com/mokitoom/pxt-phutaiRobotinput.onButtonPressed(Button.A, () => {
+    phutaiRobot.Turn(turn.Left, 50)
     basic.pause(1000)
-    IBIT.Turn(turn.Right, 50)
+    phutaiRobot.Turn(turn.Right, 50)
     basic.pause(1000)
 })
 input.onButtonPressed(Button.B, () => {
-    IBIT.Spin(spin.Left, 50)
+    phutaiRobot.Spin(spin.Left, 50)
     basic.pause(1000)
-    IBIT.Spin(spin.Right, 50)
+    phutaiRobot.Spin(spin.Right, 50)
     basic.pause(1000)
 })
 input.onButtonPressed(Button.AB, () => {
-    basic.showNumber(IBIT.ReadADC(readADC.ADC0))
-    basic.showNumber(IBIT.ReadADC(readADC.ADC1))
-    basic.showNumber(IBIT.ReadADC(readADC.ADC2))
-    basic.showNumber(IBIT.ReadADC(readADC.ADC3))
-    basic.showNumber(IBIT.ReadADC(readADC.ADC4))
-    basic.showNumber(IBIT.ReadADC(readADC.ADC5))
-    basic.showNumber(IBIT.ReadADC(readADC.ADC6))
-    basic.showNumber(IBIT.ReadADC(readADC.ADC7))
+    basic.showNumber(phutaiRobot.ReadADC(readADC.ADC0))
+    basic.showNumber(phutaiRobot.ReadADC(readADC.ADC1))
+    basic.showNumber(phutaiRobot.ReadADC(readADC.ADC2))
+    basic.showNumber(phutaiRobot.ReadADC(readADC.ADC3))
+    basic.showNumber(phutaiRobot.ReadADC(readADC.ADC4))
+    basic.showNumber(phutaiRobot.ReadADC(readADC.ADC5))
+    basic.showNumber(phutaiRobot.ReadADC(readADC.ADC6))
+    basic.showNumber(phutaiRobot.ReadADC(readADC.ADC7))
 })
 
 basic.forever(() => {
     for (let Speed = 0; Speed <= 100; Speed++) {
-        IBIT.Motor(motor.Forward, Speed)
+        phutaiRobot.Motor(motor.Forward, Speed)
         basic.pause(50)
     }
     basic.pause(1000)
     for (let Speed = 0; Speed <= 100; Speed++) {
-        IBIT.Motor(motor.Backward, Speed)
+        phutaiRobot.Motor(motor.Backward, Speed)
         basic.pause(50)
     }
     basic.pause(1000)
-    IBIT.MotorStop()
+    phutaiRobot.MotorStop()
     for (let Degree = 0; Degree <= 180; Degree++) {
-        IBIT.Servo(servo.SV1, Degree)
-        IBIT.Servo(servo.SV2, Degree)
+        phutaiRobot.Servo(servo.SV1, Degree)
+        phutaiRobot.Servo(servo.SV2, Degree)
         basic.pause(30)
     }
-    IBIT.Servo(servo.SV1, 0)
-    IBIT.Servo(servo.SV2, 0)
+    phutaiRobot.Servo(servo.SV1, 0)
+    phutaiRobot.Servo(servo.SV2, 0)
 })
